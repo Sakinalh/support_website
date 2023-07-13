1a085d589db413bd28eb7455c59277a000e1bcff
Sakina D <sa.debiche@gmail.com>
2023-07-13 20:10:15 +0200

    Add policy page

---
+++ pages/policy.js
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+export default function Privacy({ name }) {
+  return (
+    <header className="pt-20 pb-12">
+      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <p className="text-2xl dark:text-white text-center">
+      Privacy Policy for Child Application- No Information Collection
+
+At Sorobane, we prioritize the privacy and safety of children who use our application. This Privacy Policy outlines our commitment to not collecting any personal information from children under the age of 13 in accordance with the Children s Online Privacy Protection Act (COPPA) and other applicable laws and regulations.
+
+Information Collection:
+We do not collect any personal information from children using our application. Personal information refers to data that can be used to identify or contact an individual, including but not limited to names, addresses, email addresses, phone numbers, or geolocation data.
+
+Usage of Non-Personal Information:
+We may collect and use non-personal information, such as general usage statistics or anonymous aggregated data, to improve the functionality and performance of our application. This non-personal information does not identify any specific individual.
+
+Parental Consent:
+Since we do not collect personal information from children, we do not require verifiable parental consent for data collection or usage.
+
+Data Security:
+Even though we do not collect personal information, we still implement appropriate security measures to protect the non-personal information we collect. We use industry-standard practices to safeguard against unauthorized access, disclosure, alteration, or destruction of data.
+
+Third-Party Links:
+Our application may contain links to third-party websites or services. We do not have control over the content or privacy practices of these third-party sites. We encourage parents to review the privacy policies of any third-party websites or services before allowing their child to access them.
+
+Updates to Privacy Policy:
+We may update our Privacy Policy from time to time to reflect changes in our practices or legal requirements. We will notify parents about significant changes through a prominent notice within the application or via email.
+
+Contact Information:
+If you have any questions, concerns, or requests regarding our Privacy Policy, please contact us at sakina65@hotmail.com.
+
+By using our application, you acknowledge that you have read, understood, and agreed to this Privacy Policy.
+
+Last updated: 2023-07-01
+      </p>
+    </header>
+  );
+}
